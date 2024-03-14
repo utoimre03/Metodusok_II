5f4d09eae579152f9f4e25283fa0e40c98483363
utoimre03 <utoimre03@gmail.com>
2024-03-14 15:53:31 +0100

    orai munka

---
+++ src/gyakorlas0314/MetodusGyak0314.java
@@ -0,0 +1,27 @@
+
+package gyakorlas0314;
+
+public class MetodusGyak0314 {
+
+    public static void main(String[] args) {
+        tombFeltoltes();
+    }
+
+    private static void tombFeltoltes() {
+        int[] szamok = {2, 5, 7, 12, 20};
+        System.out.println(szamok);
+    }
+    
+    public static void primE(String[] args) {
+        boolean primE = false;
+        if (primE) {
+            System.out.println("Prímszám");
+        }else{
+            System.out.println("Nem prímszám");
+        }
+    }
+    
+    public static void kiir(String[] args) {
+        
+    }
+}
